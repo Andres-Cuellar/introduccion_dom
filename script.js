5df2cc2e5396fbe0b9e0d77737b7707641dd3506
Andres-Cuellar <afca133@gmail.com>
2022-07-07 16:38:15 -0500

    Se agregan comentarios al codigo

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let starValue;
 
+
+//Cumple la primera parte del reto, imprime en console.log y envia una alerta
 function messageCLG(value) {
   console.log("Haz presionado la opción: ", value);
   starValue = value;
-  console.log(starValue);
+  alert(`Haz presionado la opción: ${value}, ahora dale en SUBMIT`)
 }
 
+
+//Cumple la parte del terto de frontendmentor, donde dice la opción seleccionada, y cambia la pantalla
 function thankYou() {
   let container = document.querySelector(".container");
   container.innerHTML = `
