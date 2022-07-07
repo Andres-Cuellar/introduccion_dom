d19d0689809c84b1e1b890d26c1bac6f2c630efe
Andres-Cuellar <afca133@gmail.com>
2022-07-07 16:30:33 -0500

    se finaliza el proyecto con todos los requerimientos de frontend mentor

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let starValue;
+
 function messageCLG(value) {
-  console.log("Haz precionado la opción: ", value);
+  console.log("Haz presionado la opción: ", value);
+  starValue = value;
+  console.log(starValue);
+}
+
+function thankYou() {
+  let container = document.querySelector(".container");
+  container.innerHTML = `
+  <img src="/img/illustration-thank-you.svg" alt="thank you">  
+  <p class="container__result"> You selected out ${starValue} of 5</p>
+  <h1>Thank you!</h1>
+  <p>We appreciate you taking the time to give a rating.
+  If you ever need more support, don't hesitate to get in touch!</p>
+  `;
 }
